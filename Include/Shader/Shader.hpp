bc736dfe4b360cf7513b64b6b7814e7ec217a614
AayushBade14 <aayushrajubade@gmail.com>
2025-06-28 16:40:53 +0530

    Initial Commit: Added Shader File Header - Used for creating a shader program and setting it's uniform values.

---
+++ Include/Shader/Shader.hpp
@@ -0,0 +1,66 @@
+#pragma once
+
+#include <glad/glad.h>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+
+class Shader{
+  public:
+    Shader(const std::string &vertexPath,const std::string &fragmentPath);
+    ~Shader();
+
+    void use(); // method to use the shader program
+    
+    unsigned int getID(); 
+    
+    // method to set uniform value by reference
+    template <typename T>
+    void setValue(const std::string &name,T &value){
+      unsigned int loc = glGetUniformLocation(ID,name.c_str());
+
+      if constexpr(std::is_same<T,int>::value)
+        glUniform1i(loc,value);
+      else if constexpr(std::is_same<T,bool>::value)
+        glUniform1i(loc,(int)value);
+      else if constexpr(std::is_same<T,float>::value)
+        glUniform1f(loc,value);
+      else if constexpr(std::is_same<T,glm::vec2>::value)
+        glUniform2fv(loc,1,glm::value_ptr(value));
+      else if constexpr(std::is_same<T,glm::vec3>::value)
+        glUniform3fv(loc,1,glm::value_ptr(value));
+      else if constexpr(std::is_same<T,glm::mat4>::value)
+        glUniformMatrix4fv(loc,1,GL_FALSE,glm::value_ptr(value));
+    }
+    // method to set uniform value by value
+    template <typename T>
+    void setValue(const std::string &name,T value){
+      unsigned int loc = glGetUniformLocation(ID,name.c_str());
+
+      if constexpr(std::is_same<T,int>::value)
+        glUniform1i(loc,value);
+      else if constexpr(std::is_same<T,bool>::value)
+        glUniform1i(loc,(int)value);
+      else if constexpr(std::is_same<T,float>::value)
+        glUniform1f(loc,value);
+      else if constexpr(std::is_same<T,glm::vec2>::value)
+        glUniform2fv(loc,1,glm::value_ptr(value));
+      else if constexpr(std::is_same<T,glm::vec3>::value)
+        glUniform3fv(loc,1,glm::value_ptr(value));
+      else if constexpr(std::is_same<T,glm::mat4>::value)
+        glUniformMatrix4fv(loc,1,GL_FALSE,glm::value_ptr(value));
+    }
+
+  private:
+    unsigned int ID;
+
+    std::string loadFile(const std::string &path); // parses and loads shader file data
+    unsigned int compileShader(const std::string &src,bool isVertex); // compiles shader
+    void createShaderProgram(unsigned int &vertex,unsigned int &fragment); // creates shader prog.
+};
