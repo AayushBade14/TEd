337b0770b55e2ac28a569c81aefda85099457237
AayushBade14 <aayushrajubade@gmail.com>
2025-06-28 23:04:11 +0530

    Added collision methods

--- Include/Collision/Collision.hpp
+++ Include/Collision/Collision.hpp
@@ -23,23 +23,9 @@ typedef struct{
  ================================================================
  */
 
-bool overlapping(float minA,float maxA,float minB,float maxB){
-  return (minB <= maxA) && (minA <= maxB);
-}
+bool overlapping(float minA,float maxA,float minB,float maxB);
 
-bool rect_rect_collide(Rectangle a,Rectangle b){
-  float leftA = a.origin.x;
-  float rightA = leftA + a.size.x;
-  float bottomA = a.origin.y;
-  float topA = bottomA + a.size.y;
-
-  float leftB = b.origin.x;
-  float rightB = leftB + b.size.x;
-  float bottomB = b.origin.y;
-  float topB = bottomB + b.size.y;
-
-  return (overlapping(leftA,rightA,leftB,rightB) && overlapping(bottomA,topA,bottomB,topB));
-}
+bool rect_rect_collide(Rectangle a,Rectangle b);
 
 /*
  ================================================================
@@ -47,11 +33,4 @@ bool rect_rect_collide(Rectangle a,Rectangle b){
  ================================================================
  */
 
-bool point_rect_collide(glm::vec3 &p,Rectangle r){
-  float left = r.origin;
-  float right = left + r.size.x;
-  float bottom = r.origin.y;
-  float top = bottom + r.size.y;
-
-  return ((p.x >= left) && (p.x <= right) && (p.y >= bottom) && (p.y <= top));
-}
+bool point_rect_collide(glm::vec3 &p,Rectangle r);
