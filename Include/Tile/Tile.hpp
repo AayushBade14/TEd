a1039d9cb9125414fdb82fdbfd0b09eadab0d7f4
AayushBade14 <aayushrajubade@gmail.com>
2025-07-01 17:32:41 +0530

    Working prototype - 1.0 (Not optimized)

---
+++ Include/Tile/Tile.hpp
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <iostream>
+
+typedef struct{
+  glm::vec3 position;
+
+}Tile;
--- main.cpp
+++ main.cpp
@@ -35,6 +35,18 @@ void processInput(GLFWwindow *window){
 
 }
 
+struct Tile{
+  glm::vec3 start;
+  glm::vec3 end;
+  glm::vec3 position;
+  bool isHovered;
+};
+
+glm::vec3 start = glm::vec3(0.0f);
+glm::vec3 end = glm::vec3(0.0f);
+
+std::vector<std::vector<Tile>> tmap(5,std::vector<Tile>(5));
+
 void sel(GLFWwindow *window,Shader &shader,glm::vec3 &cursorPos){
   Rectangle r;
   r.origin = glm::vec3(0.0f);
@@ -47,10 +59,13 @@ void sel(GLFWwindow *window,Shader &shader,glm::vec3 &cursorPos){
       isHeld = true;
       p1 = cursorPos;
       p2 = cursorPos;
+      start = cursorPos;
+      end = cursorPos;
     }
-    if(isHeld)
+    if(isHeld){
       p2 = cursorPos;
-
+      end = cursorPos;
+    }
     if(isHeld && glfwGetMouseButton(window,GLFW_MOUSE_BUTTON_LEFT)==GLFW_RELEASE){
       isHeld = false;
       //p2 = cursorPos;
@@ -97,7 +112,7 @@ int main(void){
   Shader shader = Shader("./Assets/vert.glsl","./Assets/frag.glsl");
   Shader selShader = Shader("./Assets/vert.glsl","./Assets/selFrag.glsl");
   Button bt = Button("./Assets/image.jpg",origin,size);
-  Texture tex = Texture("./Assets/image.jpg");
+  Texture tex = Texture("./Assets/tile2.jpg");
   tex.setSamplerValue(shader,"buttonTex",0);
 
   VBO vbo(verticesPlane,GL_STATIC_DRAW);
@@ -109,6 +124,22 @@ int main(void){
   glfwSetFramebufferSizeCallback(window,framebuffer_size_callback);
   double xpos = WIDTH/2.0f;
   double ypos = HEIGHT/2.0f;
+  
+  float xoff = 960.0f;
+  float yoff = 540.0f;
+  for(int y = 0;y<5;y++){
+    for(int x = 0;x<5;x++){
+      float xpos = xoff + (x * 100.0f);
+      float ypos = yoff + (y*100.0f);
+
+      Tile tmp;
+      tmp.position = glm::vec3(xpos,ypos,0.0f);
+      tmp.start = start;
+      tmp.end = end;
+
+      tmap[y][x] = tmp;
+    }
+  }
 
   while(!glfwWindowShouldClose(window)){
     processInput(window);
@@ -135,18 +166,41 @@ int main(void){
     tex.unbind();
 
     model = glm::mat4(1.0f);
-    model = glm::translate(model,glm::vec3(960.0f,540.0f,0.0f));
-    model = glm::scale(model,glm::vec3(500.0f,500.0f,0.0f));
+    model = glm::translate(model,glm::vec3(550.0f,0.0f,0.0f));
+    model = glm::scale(model,glm::vec3(100.0f,100.0f,0.0f));
     tex.assignTextureUnit(0);
     selShader.use();
     selShader.setValue("model",model);
+   // selShader.setValue("model",model);
     selShader.setValue("view",view);
     selShader.setValue("projection",projection);
     sel(window,selShader,cPos); 
 
     vao.bind();
-    glDrawArrays(GL_TRIANGLES,0,36);
+    glDrawArrays(GL_TRIANGLES,0,6);
     vao.unbind();
+    
+    for(int y = 0;y<5;y++){
+      for(int x = 0;x<5;x++){
+        Rectangle r;
+        r.origin = tmap[y][x].position;
+        r.size = glm::vec3(100.0f,100.0f,0.0f);
+        if(point_rect_collide(cPos,r) && glfwGetMouseButton(window,GLFW_MOUSE_BUTTON_LEFT)==GLFW_PRESS){
+          tmap[y][x].start = start;
+          tmap[y][x].end = end;
+        }
+
+        model = glm::mat4(1.0f);
+        model = glm::translate(model,tmap[y][x].position);
+        model = glm::scale(model,glm::vec3(100.0f,100.0f,0.0f));
+        selShader.setValue("model",model);
+        selShader.setValue("p1",tmap[y][x].start);
+        selShader.setValue("p2",tmap[y][x].end);
+        vao.bind();
+        glDrawArrays(GL_TRIANGLES,0,6);
+        vao.unbind();
+      }
+    }
 
     tex.unbind();
     glfwPollEvents();
